0f0e26e1ef82a218ae2d98a5dc6390e487bce9a3
Qerogram <qerogram@naver.com>
2018-08-03 18:07:45 +0900

    Third Day, Directory Listing Detect Add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var Client = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
+var http = require('http');
 
 app.use('/assets',express.static(__dirname+'/assets'));
 app.use(bodyParser.json());
@@ -34,8 +35,14 @@ app.post('/web_list', function(req, res){
                 if(!err) _cnt = cnt;
             });
             
-            dbs.find({}, {_id:0, result:0, date:0}).toArray(function(err, result) {
+            
+            dbs.find({}, {}).toArray(function(err, result) {
                 if(err) throw err;
+                for(var i = 0; result[i] != null; ++i)  {
+                    var temp = 0;
+                    for(var j = 0; j<10; ++j) if(result[i]['result'][j]!=0) temp+=1;
+                    result[i]['count'] = temp;
+                }
                 res.send({infor : result});
             });
 
@@ -62,15 +69,50 @@ app.get('/visit_count', function(req, res) {
     });
 });
 
+p = "";
+
+ 
+function handleResponse(response) {
+  var serverData = '';
+  response.on('data', function (chunk) {
+    serverData += chunk;
+  });
+  response.on('end', function () {
+      if(serverData.indexOf("Index of") == -1) p = 1;
+      else p = 0;
+  });
+}
+ 
+
+
 app.post('/req_scan', function (req, res) {
-    Client.connect('mongodb://localhost:27017/wvas',    function(error, db){
-        if(error) console.log(error);
-        else {
-            var query = {ip:req.body.ip, count:0, grade:"A", result:[0,0,0,0,0,0,0,0,0,0], date:Date.now()};
-            var cursor = db.collection('client').insert(query);
-            db.close();
-        }
-    });
+    var ip = req.body.ip;
+    var re = /\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/;
+    if(re.test(ip) && ip.length <= 16) {
+        var options = {
+            hostname: ip,
+            port: 80,
+            path: '/'
+        };
+        
+        
+        http.request(options, function(response){
+            handleResponse(response);
+            Client.connect('mongodb://localhost:27017/wvas', function(error, db) {
+                if(error) console.log(error);
+                else {
+                    var query = {ip:ip, grade:"A", result:[p,0,0,0,0,0,0,0,0,0], date:Date.now(), count : 0};
+                    db.collection('client').update({ip:ip},query, { upsert: true }, function(err, result) {
+                        console.log(result['result']);
+                    });
+
+                    res.send({result:"success"})
+                }
+                db.close();
+            });
+        }).end();
+    }
+    else res.send({result:"fail"});
 });
 
 app.get('/scanner', function (req, res) {
