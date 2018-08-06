d331ec2fd2fdc88d9d5e57ab6149fb6228e313e3
Qerogram <qerogram@naver.com>
2018-08-06 12:49:46 +0900

    Day 6, ADD : Detail Information Page, Vuln 2(total:3)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var Client = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var http = require('http');
 var https = require('https');
+vuln = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 app.use('/assets',express.static(__dirname+'/assets'));
 app.use(bodyParser.json());
@@ -23,7 +24,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/list', function (req, res) {
-  res.sendFile(__dirname+"/list.html");
+  var idx = req.query['idx'];
+  if(isNaN(idx) == false && idx != "" && idx != " ") res.sendFile(__dirname+"/info.html");
+  else res.sendFile(__dirname+"/list.html");
 });
 
 app.post('/web_list', function(req, res){
@@ -33,11 +36,11 @@ app.post('/web_list', function(req, res){
             db.collection('client').find({}, {}).toArray(function(err, result) {
                 if(err) throw err;
                 for(var i = 0; result[i] != null; ++i)  {
-                    var temp = 0;
-                    for(var j = 0; j<10; ++j) if(result[i]['result'][j]!=0) temp+=1;
-                    result[i]['count'] = temp;
+                    var total = 0;
+                    for(var j = 0; j<10; ++j) if(result[i]['result'][j]!=0) total+=1;
+                    result[i]['count'] = total;
                     
-                    switch(temp) {
+                    switch(total) {
                         case 0 : result[i]['grade']='A'; break;
                         case 1 :
                         case 2 :
@@ -53,10 +56,9 @@ app.post('/web_list', function(req, res){
                 }
                 res.send({infor : result});
             });
-
             db.close();
         }
-    }); 
+    });
 });
 
 app.get('/load_main', function(req, res) {
@@ -83,22 +85,17 @@ app.get('/load_main', function(req, res) {
     });
 });
 
-p = "";
-
- 
-function handleResponse(response) {
+function handleResponse(response, keyword, idx) {
   var serverData = '';
   response.on('data', function (chunk) {
     serverData += chunk;
   });
   response.on('end', function () {
-      if(serverData.indexOf("Index of") == -1) p = 1;
-      else p = 0;
+      if(serverData.indexOf(keyword) == -1) vuln[idx] = 1;
+      else vuln[idx] = 0;
   });
 }
  
-
-
 app.post('/req_scan', function (req, res) {
     var ip = req.body.ip;
     var re = /^(1|2)?\d?\d([.](1|2)?\d?\d){3}$/;
@@ -108,29 +105,35 @@ app.post('/req_scan', function (req, res) {
         if(ssl == 1) re = /^(https\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
     }
     ip = ip.replace(/(^\w+:|^)\/\//, '');
-    console.log(re);
     if(re.test(ip)) {
         var options = {
             hostname: ip,
             port: 80,
             path: '/'
         };
+        
         sw = http;
+        
         if(ssl == 1) {
             options['port'] = 443;
             sw = https;
         }
-
+        
+        opt = options;
+        opt.path = "/../../../../etc/passwd";
+        
+        sw.request(opt, function(response) {
+            handleResponse(response, "../etc/passwd", 0);
+            handleResponse(response, "Port", 2);
+        }).end();
+        
         sw.request(options, function(response){
-            handleResponse(response);
+            handleResponse(response, "Index of", 1);
             Client.connect('mongodb://localhost:27017/wvas', function(error, db) {
                 if(error) console.log(error);
                 else {
-                    var query = {ip:ip, grade:"A", result:[p,0,0,0,0,0,0,0,0,0], date:Date.now(), count : 0};
-                    db.collection('client').update({ip:ip},query, { upsert: true }, function(err, result) {
-                        console.log(result['result']);
-                    });
-
+                    var query = {ip:ip, grade:"A", result:vuln, date:Date.now(), count : 0};
+                    db.collection('client').update({ip:ip},query, { upsert: true }, function(err, result) { });
                     res.send({result:"success"})
                 }
                 db.close();
@@ -144,7 +147,7 @@ app.get('/scanner', function (req, res) {
   res.sendFile(__dirname+"/scanner.html");
 });
 
-app.get('/load_infor', function(req, res) {
+app.get('/load_info', function(req, res) {
      Client.connect('mongodb://localhost:27017/wvas', function(error, db){
         if(error) console.log(error);
         else {         
@@ -163,7 +166,20 @@ app.get('/load_infor', function(req, res) {
             db.close();
         }
     }); 
-})
+});
+
+app.get('/load_target_info', function(req, res) {
+     Client.connect('mongodb://localhost:27017/wvas', function(error, db){
+        if(error) console.log(error);
+        else {         
+            db.collection('client').find({}, {_id:0, grade:0, count:0}).skip(parseInt(req.query['idx'])-1).limit(1).toArray(function(err, result) {
+                if(err) throw err;
+                res.send({"addr" : result[0]['ip'], "res" : result[0]['result'], "date": result[0]['date']});
+            });
+            db.close();
+        }
+    }); 
+});
 
 app.listen(8080, function () {
   console.log('Server Start');
