8fc34f52d5e227d985dbb570becf08763f2ecd9a
Qerogram <qerogram@naver.com>
2018-08-08 10:34:45 +0900

    Day 8, ssl check logic change, 프로젝트 종료

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -53,3 +53,8 @@
 	$(".dropdown-button").dropdown();
 	
 }(jQuery));
+
+function pad(n, width) {
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use('/assets',express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extened : true}));
 
-app.get('/', function (req, res) {
-    Client.connect('mongodb://localhost:27017/wvas', function(error, db) {
+/* Start mainPage */
+app.get('/', (req, res) => {
+    Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
         if(error) console.log(error);
         else {
             var f_query = {type : 'count', name : 'visit'};
@@ -23,17 +24,52 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/list', function (req, res) {
+app.get('/load_info', (req, res) => {
+     Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
+        if(error) console.log(error);
+        else {         
+            url_count = ip_count = 0;
+            db.collection('client').find({}, {}).toArray((err, result) => {
+                if(err) throw err;
+                for(var i = 0; result[i] != null; ++i)  {
+                    re = /^(http\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
+                    if(re.test('http://'+result[i]['ip'])) url_count++;
+                    else ip_count++;
+                }
+                total = url_count + ip_count;
+                res.send({'url_count' : url_count, 'ip_count' : ip_count, 'total' : total});
+            });
+            db.close();
+        }
+    }); 
+});
+
+app.get('/load_main', (req, res) => {
+    Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
+        if(error) console.log(error);
+        else {
+            var query = {type : 'count', name : 'visit'};
+            visit = 0;
+            db.collection('server').find(query).toArray((err, result) => visit = result[0]['value']);
+            db.collection('client').find({},{}).count((err, total) => res.send({result : true, visit : visit, total : total}));
+            db.close();
+        }
+    });
+});
+/* End mainPage */
+
+/* Start listPage */
+app.get('/list', (req, res) => {
   var idx = req.query['idx'];
   if(isNaN(idx) == false && idx != '' && idx != ' ') res.sendFile(__dirname + '/info.html');
   else res.sendFile(__dirname+'/list.html');
 });
 
-app.post('/web_list', function(req, res){
-   Client.connect('mongodb://localhost:27017/wvas', function(error, db){
+app.post('/web_list', (req, res) => {
+   Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
         if(error) console.log(error);
         else {
-            db.collection('client').find({}, {}).toArray(function(err, result) {
+            db.collection('client').find({}, {}).toArray((err, result) => {
                 if(err) throw err;
                 for(var i = 0; result[i] != null; ++i)  {
                     var total = 0;
@@ -56,25 +92,28 @@ app.post('/web_list', function(req, res){
     });
 });
 
-app.get('/load_main', function(req, res) {
-    Client.connect('mongodb://localhost:27017/wvas', function(error, db){
+app.get('/load_target_info', (req, res) => {
+     Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
         if(error) console.log(error);
-        else {
-            var query = {type : 'count', name : 'visit'};
-            var cursor = db.collection('server').find(query);
-            var visit = 0;
-            cursor.each((err, doc) => { if(!err && doc != null) visit = doc['value'] });
-            db.collection('client').find({},{}).count( (err, total) => res.send({result : true, visit : visit, total : total}));
+        else {         
+            db.collection('client').find({}, {_id:0, grade:0, count:0}).skip(parseInt(req.query['idx'])-1).limit(1).toArray((err, result) => {
+                if(err) throw err;
+                res.send({'addr' : result[0]['ip'], 'res' : result[0]['result'], 'date': result[0]['date']});
+            });
             db.close();
         }
-    });
+    }); 
 });
+/* End listPage */
 
-function handleResponse(response, keyword, idx, not) {
+/* Start requestPage */
+app.get('/scanner', (req, res) => res.sendFile(__dirname + '/scanner.html'));
+
+const handleResponse = (response, keyword, idx, not) => {
   var serverData = '';
-  response.on('data', (chunk) => serverData+=chunk);
-  response.on('end', function () {
-      if(not){
+  response.on('data', chunk => serverData+=chunk);
+  response.on('end', () => {
+      if(not) {
           if(serverData.indexOf(keyword) == -1) vuln[idx] = 1;
           else vuln[idx] = 0;
       }
@@ -85,18 +124,23 @@ function handleResponse(response, keyword, idx, not) {
   });
 }
  
-app.post('/req_scan', function (req, res) {
+app.post('/Request', (req, res) => {
     var ip = req.body.ip;
     var re = /^(1|2)?\d?\d([.](1|2)?\d?\d){3}$/;
-    var ssl = req.body.ssl;
+    ssl = result = false;
+    
     if(req.body.type=='url') {
         re = /^(http\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
-        if(ssl == 1) re = /^(https\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
+        if(!(result = re.test(ip))) {
+            re = /^(https\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
+            result = re.test(ip);
+            ssl = result;
+        }
     }
     
     ip = ip.replace(/(^\w+:|^)\/\//, '');
     
-    if(re.test(ip)) {
+    if(result) {
         var options = {
             hostname : ip,
             port : 80,
@@ -105,26 +149,23 @@ app.post('/req_scan', function (req, res) {
         
         sw = http;
         
-        if(ssl == 1) {
+        if(ssl) {
             options['port'] = 443;
             sw = https;
         }
-        
+
         sw.request(options, response => { if(response['statusCode'] == 200) vuln[3] = 1 }).end();
         
         options.path = '/../../../../etc/passwd';
-        sw.request(options, function(response) {
-            handleResponse(response, '../etc/passwd', 0, 0);
-            handleResponse(response, 'Port', 2, 0);
-        }).end();
+        sw.request(options, response => { handleResponse(response, '../etc/passwd', 0, 0); handleResponse(response, 'Port', 2, 0); }).end();
         
         options.path = '/login';
         sw.request(options, response => handleResponse(response, 'maxlength', 4, 1)).end();
         
         options.path = '/';
-        sw.request(options, function(response){
+        sw.request(options, response => {
             handleResponse(response, 'Index of', 1, 0);
-            Client.connect('mongodb://localhost:27017/wvas', function(error, db) {
+            Client.connect('mongodb://localhost:27017/wvas', (error, db) => {
                 if(error) console.log(error);
                 else {
                     var query = {ip : ip, grade : "A", result : vuln, date : Date.now(), count : 0};
@@ -138,39 +179,6 @@ app.post('/req_scan', function (req, res) {
     else res.send({result : 'fail'});
 });
 
-app.get('/scanner', (req, res) => res.sendFile(__dirname + '/scanner.html'));
-
-app.get('/load_info', function(req, res) {
-     Client.connect('mongodb://localhost:27017/wvas', function(error, db){
-        if(error) console.log(error);
-        else {         
-            url_count = ip_count = 0;
-            db.collection('client').find({}, {}).toArray(function(err, result) {
-                if(err) throw err;
-                for(var i = 0; result[i] != null; ++i)  {
-                    re = /^(http\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
-                    if(re.test('http://'+result[i]['ip'])) url_count++;
-                    else ip_count++;
-                }
-                total = url_count + ip_count;
-                res.send({"url_count" : url_count, "ip_count" : ip_count, "total" : total});
-            });
-            db.close();
-        }
-    }); 
-});
-
-app.get('/load_target_info', function(req, res) {
-     Client.connect('mongodb://localhost:27017/wvas', function(error, db){
-        if(error) console.log(error);
-        else {         
-            db.collection('client').find({}, {_id:0, grade:0, count:0}).skip(parseInt(req.query['idx'])-1).limit(1).toArray(function(err, result) {
-                if(err) throw err;
-                res.send({"addr" : result[0]['ip'], "res" : result[0]['result'], "date": result[0]['date']});
-            });
-            db.close();
-        }
-    }); 
-});
+/* End requestPage */
 
 app.listen(8080, () => console.log('Server Start'));
